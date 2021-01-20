367d9a50c5ab434af4246506e7344ca490e3b62c
Wyatt Johnson <accounts+github@wyattjoh.ca>
2021-01-20 10:24:02 -0700

    fix: remove resetting logic

--- common/coral/cursor.go
+++ common/coral/cursor.go
@@ -7,7 +7,6 @@ import (
 
 var createdAtCounter struct {
 	enabled bool
-	last    int64
 	counter time.Duration
 	mutex   sync.Mutex
 }
@@ -21,26 +20,14 @@ func EnableMonotonicCursorTime() {
 // NewCursorTime is used to return time values that are used by pagination
 // fields.
 func NewCursorTime() Time {
-	now := time.Now()
-
 	if createdAtCounter.enabled {
-		unix := now.Unix()
-
 		// We increment the milliseconds value for each unique time.
 		createdAtCounter.mutex.Lock()
 
 		var counter time.Duration
 
-		// If the unix timestamp is the same as the last one, then we need to
-		// increment the counter. If it's not, then we need to reset the counter.
-		if createdAtCounter.last == unix {
-			createdAtCounter.counter += 1 * time.Millisecond
-			counter = createdAtCounter.counter
-		} else {
-			createdAtCounter.last = unix
-			createdAtCounter.counter = 0
-			counter = 0
-		}
+		createdAtCounter.counter += 1 * time.Millisecond
+		counter = createdAtCounter.counter
 
 		createdAtCounter.mutex.Unlock()
 
